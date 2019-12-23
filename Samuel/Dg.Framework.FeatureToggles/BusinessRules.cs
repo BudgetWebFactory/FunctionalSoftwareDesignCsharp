6f185f3d3b206bb38460cb3f4aee8fc64fd520c9
Samuel Bittmann <samuelbittmann@digitecgalaxus.ch>
2019-12-23 15:49:34 +0100

    [Samuel] Added pure annotation

--- Samuel/Dg.Framework.FeatureToggles/BusinessRules.cs
+++ Samuel/Dg.Framework.FeatureToggles/BusinessRules.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics.Contracts;
+
 namespace Dg.Framework.FeatureToggles
 {
     internal static class BusinessRules
     {
+        [Pure]
         public static bool IsActiveForUser(FeatureToggle featureToggle, int userId) =>
             featureToggle.IsActive && featureToggle.ActivatedUsers.Contains(userId);
     }
