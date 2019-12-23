e7f2e33ab0607ae3dc98d62c9f0fae3d0682a133
Samuel Bittmann <samuelbittmann@digitecgalaxus.ch>
2019-12-23 14:45:31 +0100

    [Samuel] Refactored feature toggle api to use result monad

--- Samuel/Dg.Framework.FeatureToggles/Api/FeatureToggleApiController.cs
+++ Samuel/Dg.Framework.FeatureToggles/Api/FeatureToggleApiController.cs
@@ -1,27 +1,27 @@
-using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Chabis.Functional;
 using static Dg.Framework.FeatureToggles.Persistence;
 using static Dg.Framework.FeatureToggles.Transformations;
-
+using System.Net;
 
 namespace Dg.Framework.FeatureToggles.Api
 {
     public class FeatureToggleApiController : ControllerBase
     {
         [Route("v1/featureToggles/{featureToggleId}/users/{userId}")]
-        public IActionResult Get([FromRoute] string featureToggleId, [FromRoute] int userId)
+        public IActionResult Get([FromRoute] string featureToggleId, [FromRoute] int userId) =>
+            GetFeatureToggle(featureToggleId)
+                .Map(ft => ToActiveStatusForUser(ft, userId))
+                .Map(a => new FeatureToggleResponse(a.FeatureToggleId, a.IsActive))
+                .Match<IActionResult>(Ok, err => StatusCode((int)err));
+
+        private static Result<FeatureToggle, HttpStatusCode> GetFeatureToggle(string featureToggleId)
         {
-            var featureToggles = LoadAll().Where(ft => ft.Id == featureToggleId).ToList();
-            var ft = featureToggles.Count() == 1 ? (FeatureToggle?)featureToggles[0] : null;
-            if (ft == null)
-            {
-                return NotFound();
-            }
-
-            var activeStatus = ToActiveStatusForUser(ft.Value, userId);
-
-            return Ok(new FeatureToggleResponse(activeStatus.FeatureToggleId, activeStatus.IsActive));
+            var featureToggle = LoadAll().Where(ft => ft.Id == featureToggleId).ToList();
+            return featureToggle.Count == 1
+                ? (Result<FeatureToggle, HttpStatusCode>)featureToggle[0]
+                : HttpStatusCode.NotFound;
         }
     }
 }
